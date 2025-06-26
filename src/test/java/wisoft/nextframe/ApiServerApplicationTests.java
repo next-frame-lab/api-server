649fadf75a9882437587bbe005bdeff087cfcb55
Jinpyo An <dkswlsvy3312@gmail.com>
2025-06-26 15:45:28 +0900

    initial project setup

---
+++ src/test/java/wisoft/nextframe/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wisoft.nextframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
