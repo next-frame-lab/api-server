1e599459eee5839888aa6afbd4b6bab14a3a91f0
Minseo Kim <kms12782@gmail.com>
2025-08-12 11:14:48 +0900

    #24 feat(ticketing): 티켓 발급 중복 방지 예외 추가 및 발급 로직
    - TicketService에 AlreadyIssuedException 추가하여 동일 예약에 대한 중복 발급 방지
    - Ticket 엔티티 발급 로직 수정
      - seatId, scheduleId 미구현 상태이므로 TODO 주석 추가
      - 발급 시 필드 null-check 처리
    - TicketService에서 발급 전 중복 여부 확인 로직 적용

---
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/ticketing/service/AlreadyIssuedException.java
@@ -0,0 +1,9 @@
+package wisoft.nextframe.schedulereservationticketing.ticketing.service;
+
+import wisoft.nextframe.schedulereservationticketing.reservation.domain.ReservationId;
+
+public class AlreadyIssuedException extends RuntimeException {
+	public AlreadyIssuedException(ReservationId reservationId) {
+		super("이미 발급된 티켓입니다. 예약 ID: " + reservationId.getValue());
+	}
+}
