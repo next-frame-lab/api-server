924580b1a0d48e085923e13e53ba64467e7843bf
Minseo Kim <kms12782@gmail.com>
2025-08-12 11:29:00 +0900

    #24 refector: root src 파일 제거

--- src/main/java/wisoft/nextframe/ticketing/application/port/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package wisoft.nextframe.ticketing.application.port;
-
-import java.util.UUID;
-
-import wisoft.nextframe.ticketing.domain.Ticket;
-
-public interface TicketRepository {
-	Ticket save(Ticket ticket, UUID reservationId, UUID seatId, UUID scheduleId);
-}
