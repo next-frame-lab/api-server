29cb89c67bf36edb3b055068c5ebcc3a74f8c919
Jinpyo An <dkswlsvy3312@gmail.com>
2025-08-13 16:30:17 +0900

    #30 feat(performance): 공연 상세 조회 Controller, 예외 처리기 추가
    공연 상세 조회 API Controller를 구현합니다.
    에러 발생 시 404, 400, 500 응답을 반환하도록
    GlobalExceptionHandler를 설정합니다.
    
    Resolves: #30
    See also: None

---
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/controller/performance/PerformanceController.java
@@ -0,0 +1,28 @@
+package wisoft.nextframe.schedulereservationticketing.controller.performance;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import wisoft.nextframe.schedulereservationticketing.common.response.ApiResponse;
+import wisoft.nextframe.schedulereservationticketing.dto.performance.PerformanceDetailResponse;
+import wisoft.nextframe.schedulereservationticketing.service.performance.PerformanceService;
+
+@RestController
+@RequestMapping("/api/v1/performances")
+@RequiredArgsConstructor
+public class PerformanceController {
+
+	private final PerformanceService performanceService;
+
+	@GetMapping("/{id}")
+	public ApiResponse<PerformanceDetailResponse> getPerformanceDetail(@PathVariable UUID id) {
+		final PerformanceDetailResponse data = performanceService.getPerformanceDetail(id);
+
+		return ApiResponse.success(data);
+	}
+}
