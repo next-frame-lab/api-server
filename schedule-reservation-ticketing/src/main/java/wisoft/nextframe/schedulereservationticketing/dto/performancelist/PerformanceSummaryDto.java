96f5355522ca2a1e00ae706622facd9fcd7aba19
Jinpyo An <dkswlsvy3312@gmail.com>
2025-08-16 15:39:31 +0900

    #36 feat(performance): 공연 목록 조회 Repository 구현 및 테스트

--- schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/dto/performancelist/PerformanceSummaryDto.java
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/dto/performancelist/PerformanceSummaryDto.java
@@ -1,15 +1,15 @@
 package wisoft.nextframe.schedulereservationticketing.dto.performancelist;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.UUID;
 
-import lombok.Builder;
 import lombok.Getter;
 import wisoft.nextframe.schedulereservationticketing.entity.performance.PerformanceGenre;
 import wisoft.nextframe.schedulereservationticketing.entity.performance.PerformanceType;
 
 @Getter
-@Builder
 public class PerformanceSummaryDto {
 
 	private final UUID id;
@@ -22,18 +22,19 @@ public class PerformanceSummaryDto {
 	private final LocalDate endDate;
 	private final Boolean adultOnly;
 
-	// JPQL의 SELECT NEW 구문에서 사용할 생성자
+	// JPQL의 'SELECT NEW' 구문에서 사용할 생성자
 	public PerformanceSummaryDto(UUID id, String name, String imageUrl, PerformanceType type,
 		PerformanceGenre genre, String stadiumName,
-		LocalDate startDate, LocalDate endDate, Boolean adultOnly) {
+		// 파라미터 타입을 java.util.Date로 변경
+		Date startDate, Date endDate, Boolean adultOnly) {
 		this.id = id;
 		this.name = name;
 		this.imageUrl = imageUrl;
 		this.type = type;
 		this.genre = genre;
 		this.stadiumName = stadiumName;
-		this.startDate = startDate;
-		this.endDate = endDate;
+		this.startDate = ((java.sql.Date) startDate).toLocalDate();
+		this.endDate = ((java.sql.Date) endDate).toLocalDate();
 		this.adultOnly = adultOnly;
 	}
 }
