833ce9a6cf56fbc2e526ba20722858b970be4242
Jinpyo An <dkswlsvy3312@gmail.com>
2025-08-20 15:35:54 +0900

    #40 feat(reservation): 공연 예매 API에 필요한 DTO 정의

---
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/dto/reservation/response/PerformanceInfo.java
@@ -0,0 +1,16 @@
+package wisoft.nextframe.schedulereservationticketing.dto.reservation.response;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PerformanceInfo {
+
+	private final String name;
+	private final LocalDate scheduleDate;
+	private final LocalTime scheduleTime;
+}
