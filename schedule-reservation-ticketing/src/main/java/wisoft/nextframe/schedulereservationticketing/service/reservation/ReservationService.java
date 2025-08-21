48f78470dc843848cb78b28307afe5781c08e260
Jinpyo An <dkswlsvy3312@gmail.com>
2025-08-21 16:25:44 +0900

    #40 refactor(reservation): ReservationService의 엔티티 조회 및 검증 로직을 ReservationDataProvider로 분리

--- schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/service/reservation/ReservationService.java
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/service/reservation/ReservationService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import wisoft.nextframe.schedulereservationticketing.dto.reservation.request.ReservationRequest;
 import wisoft.nextframe.schedulereservationticketing.dto.reservation.response.ReservationResponse;
@@ -14,45 +13,36 @@ import wisoft.nextframe.schedulereservationticketing.entity.reservation.Reservat
 import wisoft.nextframe.schedulereservationticketing.entity.schedule.Schedule;
 import wisoft.nextframe.schedulereservationticketing.entity.stadium.SeatDefinition;
 import wisoft.nextframe.schedulereservationticketing.entity.user.User;
-import wisoft.nextframe.schedulereservationticketing.exception.reservation.SeatNotDefinedException;
 import wisoft.nextframe.schedulereservationticketing.exception.reservation.TotalPriceMismatchException;
 import wisoft.nextframe.schedulereservationticketing.repository.reservation.ReservationRepository;
-import wisoft.nextframe.schedulereservationticketing.repository.schedule.ScheduleRepository;
 import wisoft.nextframe.schedulereservationticketing.repository.seat.SeatStateRepository;
-import wisoft.nextframe.schedulereservationticketing.repository.stadium.SeatDefinitionRepository;
-import wisoft.nextframe.schedulereservationticketing.repository.user.UserRepository;
 
 @RequiredArgsConstructor
 @Service
 public class ReservationService {
 
-	private final UserRepository userRepository;
-	private final ScheduleRepository scheduleRepository;
-	private final SeatDefinitionRepository seatDefinitionRepository;
 	private final SeatStateRepository seatStateRepository;
 	private final ReservationRepository reservationRepository;
 	private final PriceCalculator priceCalculator;
 	private final ReservationFactory reservationFactory;
 	private final ReservationMapper reservationMapper;
+	private final ReservationDataProvider dataProvider;
 
 	@Transactional
 	public ReservationResponse reserveSeat(ReservationRequest request) {
-		// 1. 엔티티를 조회 및 검증합니다.
-		final User user = userRepository.findById(request.getUserId())
-			.orElseThrow(EntityNotFoundException::new);
-		final Schedule schedule = scheduleRepository.findById(request.getScheduleId())
-			.orElseThrow(EntityNotFoundException::new);
-		final List<SeatDefinition> seats = seatDefinitionRepository.findWithStadiumSectionByIdIn(request.getSeatIds());
-		if (seats.size() != request.getSeatIds().size()) {
-			throw new SeatNotDefinedException("요청한 좌석 중 일부를 찾을 수 없습니다.");
-		}
+		// 1. 데이터 준비
+		final ReservationContext context = dataProvider.provide(request);
+		final Performance performance = context.performance();
+		final User user = context.user();
+		final List<SeatDefinition> seats = context.seats();
+		final Schedule schedule = context.schedule();
 
 		// 2. 공연 연령 제한에 대한 사용자를 검증합니다.
-		final Performance performance = schedule.getPerformance();
 		performance.verifyAgeLimit(user);
 
 		// 3. 사용자가 선택한 좌석의 총 금액을 계산합니다.
 		final int calculatedTotalPrice = priceCalculator.calculateTotalPrice(performance, seats);
+
 		// 4. 요청 금액(클라이언트)과 계산 금액이 일치하는지 검증합니다.
 		if (calculatedTotalPrice != request.getTotalAmount()) {
 			throw new TotalPriceMismatchException("요청된 금액과 계산된 금액이 일치하지 않습니다.");
