6c1fdca672902857ccd2d24f8821116c6ceeda3d
Jinpyo An <dkswlsvy3312@gmail.com>
2025-08-24 18:40:13 +0900

    #42 feat(stadium): 공연 좌석 배치도 API Controller 정의

---
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/controller/stadium/StadiumController.java
@@ -0,0 +1,32 @@
+package wisoft.nextframe.schedulereservationticketing.controller.stadium;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import wisoft.nextframe.schedulereservationticketing.common.response.ApiResponse;
+import wisoft.nextframe.schedulereservationticketing.dto.seatdefinition.SeatDefinitionListResponse;
+import wisoft.nextframe.schedulereservationticketing.service.stadium.StadiumService;
+
+@RestController
+@RequestMapping("/api/v1/stadiums")
+@RequiredArgsConstructor
+public class StadiumController {
+
+	private final StadiumService stadiumService;
+
+	@GetMapping("/{id}/seat-definitions")
+	public ResponseEntity<ApiResponse<?>> getSeatDefinitions(@PathVariable UUID id) {
+		final SeatDefinitionListResponse data = stadiumService.getSeatDefinitions(id);
+
+		final ApiResponse<SeatDefinitionListResponse> response = ApiResponse.success(data);
+
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+}
