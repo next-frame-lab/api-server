695f4fb8344b31c689686ab35b1b98f7a82fa8c7
Jinpyo An <dkswlsvy3312@gmail.com>
2025-08-24 18:26:05 +0900

    #42 feat(stadium): 공연 좌석 배치도 API Repository 정의
    ; Conflicts:
    ;	schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/repository/stadium/SeatDefinitionRepository.java
    
    diff --git c/schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/repository/stadium/SeatDefinitionRepository.java i/schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/repository/stadium/SeatDefinitionRepository.java
    index 26cee22..07d11aa 100644
    --- c/schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/repository/stadium/SeatDefinitionRepository.java
    +++ i/schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/repository/stadium/SeatDefinitionRepository.java
    @@ -11,6 +11,13 @@ import wisoft.nextframe.schedulereservationticketing.entity.stadium.SeatDefiniti
    
     public interface SeatDefinitionRepository extends JpaRepository<SeatDefinition, UUID> {
    
    +	@Query(value = """
    +				select sd from SeatDefinition sd
    +				join fetch sd.stadiumSection ss
    +				order by ss.section asc, sd.rowNo asc, sd.columnNo asc
    +		""")
    +	List<SeatDefinition> findAllByStadiumIdWithSorting(@Param("stadiumId") UUID stadiumId);
    +
     	/**
     	 * 여러 좌석 ID에 해당하는 좌석 정보를 모두 조회합니다. (N+1 문제 방지를 위해 fetch join 사용)
     	 * @param seatIds 좌석 ID 목록

--- schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/repository/stadium/SeatDefinitionRepository.java
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/repository/stadium/SeatDefinitionRepository.java
@@ -11,6 +11,13 @@ import wisoft.nextframe.schedulereservationticketing.entity.stadium.SeatDefiniti
 
 public interface SeatDefinitionRepository extends JpaRepository<SeatDefinition, UUID> {
 
+	@Query(value = """
+				select sd from SeatDefinition sd
+				join fetch sd.stadiumSection ss
+				order by ss.section asc, sd.rowNo asc, sd.columnNo asc 
+		""")
+	List<SeatDefinition> findAllByStadiumIdWithSorting(@Param("stadiumId") UUID stadiumId);
+
 	/**
 	 * 여러 좌석 ID에 해당하는 좌석 정보를 모두 조회합니다. (N+1 문제 방지를 위해 fetch join 사용)
 	 * @param seatIds 좌석 ID 목록
