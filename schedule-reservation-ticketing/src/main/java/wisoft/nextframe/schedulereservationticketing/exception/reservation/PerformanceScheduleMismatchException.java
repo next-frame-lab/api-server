2c04d73c2916b8e0194368fdf701cbe1efacbc0b
Jinpyo An <dkswlsvy3312@gmail.com>
2025-08-27 12:58:42 +0900

    #49 refactor(reservation): 공연 좌석 예매 API - 예매 관련 예외 처리 구체화

---
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/exception/reservation/PerformanceScheduleMismatchException.java
@@ -0,0 +1,7 @@
+package wisoft.nextframe.schedulereservationticketing.exception.reservation;
+
+public class PerformanceScheduleMismatchException extends ReservationException {
+	public PerformanceScheduleMismatchException(String message) {
+		super(message);
+	}
+}
