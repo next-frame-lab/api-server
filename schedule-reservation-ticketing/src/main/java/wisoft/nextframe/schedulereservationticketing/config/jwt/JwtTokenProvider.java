0970c21d92b777b9fda02e9260be7eb942fe4f8b
Jinpyo An <dkswlsvy3312@gmail.com>
2025-09-03 14:44:36 +0900

    #52 feat(signin): 소셜 로그인(kakao) API - JWT 설정 및 생성 유틸리티 JwtTokenProvider 구현
    diff --git c/schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/config/jwt/JwtTokenProvider.java i/schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/config/jwt/JwtTokenProvider.java
    new file mode 100644
    index 0000000..42bfa0e
    --- /dev/null
    +++ i/schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/config/jwt/JwtTokenProvider.java
    @@ -0,0 +1,52 @@
    +package wisoft.nextframe.schedulereservationticketing.config.jwt;
    +
    +
    +import java.security.Key;
    +import java.util.Date;
    +import java.util.UUID;
    +
    +import org.springframework.beans.factory.annotation.Value;
    +import org.springframework.stereotype.Component;
    +
    +import io.jsonwebtoken.Jwts;
    +import io.jsonwebtoken.SignatureAlgorithm;
    +import io.jsonwebtoken.io.Decoders;
    +import io.jsonwebtoken.security.Keys;
    +
    +@Component
    +public class JwtTokenProvider {
    +
    +	private final Key key;
    +	private final long accessTokenExpireTime;
    +	private final long refreshTokenExpireTime;
    +
    +	public JwtTokenProvider(
    +		@Value("${jwt.secret-key}") String secretKey,
    +		@Value("${jwt.access-token-expire-time}") long accessTokenExpireTime,
    +		@Value("${jwt.refresh-token-expire-time}") long refreshTokenExpireTime) {
    +		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
    +		this.key = Keys.hmacShaKeyFor(keyBytes);
    +		this.accessTokenExpireTime = accessTokenExpireTime;
    +		this.refreshTokenExpireTime = refreshTokenExpireTime;
    +	}
    +
    +	public String generateAccessToken(UUID userId) {
    +		return generateToken(userId, accessTokenExpireTime);
    +	}
    +
    +	public String generateRefreshToken(UUID userId) {
    +		return generateToken(userId, refreshTokenExpireTime);
    +	}
    +
    +	private String generateToken(UUID userId, long expireTime) {
    +		final Date now = new Date();
    +		final Date expiryDate = new Date(now.getTime() + expireTime);
    +
    +		return Jwts.builder()
    +			.setSubject(userId.toString()) // 토큰의 주체로 사용자 ID(UUID)를 문자열로 사용
    +			.setIssuedAt(now)
    +			.setExpiration(expiryDate)
    +			.signWith(key, SignatureAlgorithm.HS512)
    +			.compact();
    +	}
    +}

---
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/config/jwt/JwtTokenProvider.java
@@ -0,0 +1,52 @@
+package wisoft.nextframe.schedulereservationticketing.config.jwt;
+
+
+import java.security.Key;
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtTokenProvider {
+
+	private final Key key;
+	private final long accessTokenExpireTime;
+	private final long refreshTokenExpireTime;
+
+	public JwtTokenProvider(
+		@Value("${jwt.secret-key}") String secretKey,
+		@Value("${jwt.access-token-expire-time}") long accessTokenExpireTime,
+		@Value("${jwt.refresh-token-expire-time}") long refreshTokenExpireTime) {
+		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+		this.key = Keys.hmacShaKeyFor(keyBytes);
+		this.accessTokenExpireTime = accessTokenExpireTime;
+		this.refreshTokenExpireTime = refreshTokenExpireTime;
+	}
+
+	public String generateAccessToken(UUID userId) {
+		return generateToken(userId, accessTokenExpireTime);
+	}
+
+	public String generateRefreshToken(UUID userId) {
+		return generateToken(userId, refreshTokenExpireTime);
+	}
+
+	private String generateToken(UUID userId, long expireTime) {
+		final Date now = new Date();
+		final Date expiryDate = new Date(now.getTime() + expireTime);
+
+		return Jwts.builder()
+			.setSubject(userId.toString()) // 토큰의 주체로 사용자 ID(UUID)를 문자열로 사용
+			.setIssuedAt(now)
+			.setExpiration(expiryDate)
+			.signWith(key, SignatureAlgorithm.HS512)
+			.compact();
+	}
+}
