4ce60de4bf7caa4991c538a8d7addbb7ed163c34
Jinpyo An <dkswlsvy3312@gmail.com>
2025-09-04 17:38:16 +0900

    #52 feat(signin): access token 재발급 API - DTO 구현

---
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/dto/auth/tokenrefresh/TokenRefreshRequest.java
@@ -0,0 +1,8 @@
+package wisoft.nextframe.schedulereservationticketing.dto.auth.tokenrefresh;
+
+import lombok.Getter;
+
+@Getter
+public class TokenRefreshRequest {
+	private String refreshToken;
+}
---
+++ schedule-reservation-ticketing/src/main/java/wisoft/nextframe/schedulereservationticketing/dto/auth/tokenrefresh/TokenRefreshResponse.java
@@ -0,0 +1,10 @@
+package wisoft.nextframe.schedulereservationticketing.dto.auth.tokenrefresh;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor // 생성자를 통해 accessToken 값을 받도록 설정
+public class TokenRefreshResponse {
+	private String accessToken;
+}
