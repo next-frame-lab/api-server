abe62518724425e12a74981a7c0269304f71c0a9
Minseo Kim <kms12782@gmail.com>
2025-09-08 11:33:38 +0900

    fix payment 서버에서 schedule-reservation-ticketing 서버 분리

--- payment/src/main/java/wisoft/nextframe/payment/PaymentApplication.java
+++ payment/src/main/java/wisoft/nextframe/payment/PaymentApplication.java
@@ -2,12 +2,8 @@ package wisoft.nextframe.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {
-	"wisoft.nextframe.payment",
-	"wisoft.nextframe.schedulereservationticketing"})
 public class PaymentApplication {
 
 	public static void main(String[] args) {
