6f8db4987054f85312a342a081f242e4ae65924d
Jinpyo An <dkswlsvy3312@gmail.com>
2025-09-10 14:13:42 +0900

    ...

--- schedule-reservation-ticketing/src/test/java/wisoft/nextframe/schedulereservationticketing/ScheduleReservationTicketingApplicationTests.java
+++ schedule-reservation-ticketing/src/test/java/wisoft/nextframe/schedulereservationticketing/ScheduleReservationTicketingApplicationTests.java
@@ -1,10 +1,8 @@
 package wisoft.nextframe.schedulereservationticketing;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 class ScheduleReservationTicketingApplicationTests {
 
