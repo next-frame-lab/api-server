f9db1fd144aad812042f18f16cc3e5eb8770559b
Jinpyo An <dkswlsvy3312@gmail.com>
2025-09-10 12:57:23 +0900

    ..

--- schedule-reservation-ticketing/src/test/java/wisoft/nextframe/schedulereservationticketing/ScheduleReservationTicketingApplicationTests.java
+++ schedule-reservation-ticketing/src/test/java/wisoft/nextframe/schedulereservationticketing/ScheduleReservationTicketingApplicationTests.java
@@ -1,16 +1,10 @@
 package wisoft.nextframe.schedulereservationticketing;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(properties = {
-	"spring.datasource.url=jdbc:postgresql://postgres:5432/testdb",
-	"spring.datasource.username=testuser",
-	"spring.datasource.password=testpassword",
-	"spring.datasource.driver-class-name=org.postgresql.Driver",
-	"spring.jpa.hibernate.ddl-auto=create-drop" // 테스트 환경에서는 validate 대신 create-drop이 안전합니다.
-})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 class ScheduleReservationTicketingApplicationTests {
 
