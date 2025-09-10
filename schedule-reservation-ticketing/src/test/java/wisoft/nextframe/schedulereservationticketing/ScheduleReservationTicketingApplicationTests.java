4279b5c49da2678d0434e224c5c26e4bddd50368
Jinpyo An <dkswlsvy3312@gmail.com>
2025-09-10 12:07:47 +0900

    #55 chore: 불필요한 설정 제거

--- schedule-reservation-ticketing/src/test/java/wisoft/nextframe/schedulereservationticketing/ScheduleReservationTicketingApplicationTests.java
+++ schedule-reservation-ticketing/src/test/java/wisoft/nextframe/schedulereservationticketing/ScheduleReservationTicketingApplicationTests.java
@@ -2,9 +2,7 @@ package wisoft.nextframe.schedulereservationticketing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(locations = "classpath:application.yml")
 @SpringBootTest
 class ScheduleReservationTicketingApplicationTests {
 
