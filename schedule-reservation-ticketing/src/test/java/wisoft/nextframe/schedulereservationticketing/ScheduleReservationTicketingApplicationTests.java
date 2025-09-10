a6c6762a10f786e10dc4dd22a2e42862ec662bb8
Jinpyo An <dkswlsvy3312@gmail.com>
2025-09-10 12:11:08 +0900

    .

--- schedule-reservation-ticketing/src/test/java/wisoft/nextframe/schedulereservationticketing/ScheduleReservationTicketingApplicationTests.java
+++ schedule-reservation-ticketing/src/test/java/wisoft/nextframe/schedulereservationticketing/ScheduleReservationTicketingApplicationTests.java
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = {
+	"spring.datasource.url=jdbc:postgresql://postgres:5432/testdb",
+	"spring.datasource.username=testuser",
+	"spring.datasource.password=testpassword",
+	"spring.datasource.driver-class-name=org.postgresql.Driver",
+	"spring.jpa.hibernate.ddl-auto=create-drop" // 테스트 환경에서는 validate 대신 create-drop이 안전합니다.
+})
 @SpringBootTest
 class ScheduleReservationTicketingApplicationTests {
 
