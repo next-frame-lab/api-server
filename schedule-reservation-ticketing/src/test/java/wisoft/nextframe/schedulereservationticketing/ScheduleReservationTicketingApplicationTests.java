ce5d06db1dcb6ea526f9dbd486047efca1e37be5
Jinpyo An <dkswlsvy3312@gmail.com>
2025-09-11 12:03:55 +0900

    #55 chore: testcontainers가 로드가 안되는 문제 해결

--- schedule-reservation-ticketing/src/test/java/wisoft/nextframe/schedulereservationticketing/ScheduleReservationTicketingApplicationTests.java
+++ schedule-reservation-ticketing/src/test/java/wisoft/nextframe/schedulereservationticketing/ScheduleReservationTicketingApplicationTests.java
@@ -1,10 +1,10 @@
 package wisoft.nextframe.schedulereservationticketing;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ScheduleReservationTicketingApplicationTests {
+import wisoft.nextframe.schedulereservationticketing.config.AbstractIntegrationTest;
+
+class ScheduleReservationTicketingApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
